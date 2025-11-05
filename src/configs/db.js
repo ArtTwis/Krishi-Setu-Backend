eb659ac3545ec403a00e8cf0c9a542a743340c45
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2025-11-06 02:02:00 +0530

    added db config file

---
+++ src/configs/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants/common.js";
+
+const connectDb = async () => {
+  try {
+    //   const mongoDbConnectionInstance = await mongoose.connect(
+    //     `${process.env.MONGODB_URI}/${DB_NAME}`
+    //   );
+    //   console.log(
+    //     `\nConnected to MongoDb successfully, DB host :`,
+    //     mongoDbConnectionInstance.connection.host
+    //   );
+  } catch (error) {
+    console.log("\nError while connecting to db :", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
